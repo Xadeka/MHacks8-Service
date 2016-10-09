441c666eaf454a0f4efcad08d6702ddda9519ad7
Zade Kaakarni <xade.kaakarni@gmail.com>
2016-10-09 00:06:09 -0400

    switching route syntax

--- application.py
+++ application.py
@@ -2,11 +2,12 @@ from flask import Flask
 
 application = Flask(__name__)
 
+@application.route('/alexa', methods=['GET', 'POST'])
 def ask_alexa():
     return 'I am processing your request'
 
-application.add_url_rule('/alexa', '', (lambda:
-    ask_alexa()))
+# application.add_url_rule('/alexa', '', (lambda:
+#    ask_alexa()))
 
 # run the app.
 if __name__ == "__main__":
