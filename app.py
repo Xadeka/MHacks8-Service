c25438c6667c51c172d88ea1b14191baa7152e19
Zade Kaakarni <xade.kaakarni@gmail.com>
2016-10-08 23:37:09 -0400

    minor updates

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/alexa', methods=['GET', 'POST'])
 def ask_alexa():
     return 'I am processing your request'
+
+application.add_url_rule('/alexa', '', (lambda:
+    ask_alexa()))
 
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    app.debug = True
-    app.run()
+    application.debug = True
+    application.run()
